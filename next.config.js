d4a8cf52b4ddcaa02313c8a891368ff7542a9ba2
Santiago Aguirre Giraldo <santiago@Santiagos-MacBook-Pro.local>
2023-05-09 09:03:53 -0500

    pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ['placeimg.com', 'api.lorem.space', 'i.picsum.photos', 'api.escuelajs.co', 'picsum.photos', 'images.unsplash.com', 'empresas.blogthinkbig.com', 'img.freepik.com', 'www.woodenson.co', 'emprendepyme.net', 'images.pexels.com', 't3.gstatic.com', 'www.pixel-creation.com'],
   },
 };
-module.exports = nextConfig
+
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+});
+
+module.exports = withPWA(nextConfig);
